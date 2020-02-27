207b1af87b0b34e7d8c9a6a6619daebf428e01d1
togai-r <togai-r@klab.com>
2020-02-27 11:40:14 +0900

    fix: bug fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name="ttt",
-    version="0.0.1",
+    version="0.0.2",
     packages=['ttt'],
     description='Togai Tsv Tool',
     long_description=readme,
